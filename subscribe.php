fec4e9ef8229a76355e62d57369f51f68fe6ee4c
lucky091588 <lucky091588@gmail.com>
2021-04-15 17:34:41 +0800

    Regenerate module and add postInstall hook

--- subscribe.php
+++ subscribe.php
@@ -30,6 +30,15 @@ function subscribe_civicrm_install() {
 }
 
 /**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function subscribe_civicrm_postInstall() {
+  _subscribe_civix_civicrm_postInstall();
+}
+
+/**
  * Implements hook_civicrm_uninstall().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
