845309e7bc91e62dfb9d97498d8d119110fd85d0
Allen Shaw <allen@JoineryHQ.com>
2026-03-20 18:21:20 -0500

    civix upgrade, format: 25.10.2

--- subscribe.php
+++ subscribe.php
@@ -12,15 +12,6 @@ function subscribe_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function subscribe_civicrm_xmlMenu(&$files) {
-  _subscribe_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -30,81 +21,10 @@ function subscribe_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function subscribe_civicrm_postInstall() {
-  _subscribe_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function subscribe_civicrm_uninstall() {
-  return _subscribe_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function subscribe_civicrm_enable() {
   return _subscribe_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function subscribe_civicrm_disable() {
-  return _subscribe_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function subscribe_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _subscribe_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function subscribe_civicrm_managed(&$entities) {
-  return _subscribe_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function subscribe_civicrm_caseTypes(&$caseTypes) {
-  _subscribe_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function subscribe_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _subscribe_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
